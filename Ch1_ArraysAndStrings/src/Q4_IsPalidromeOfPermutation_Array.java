f9125a68206a0d687093fe5a6046399be72b4292
AShiou <aa8693@gmail.com>
2020-01-09 15:04:19 +0800

    Ch1-Q4 isPalidromePermutation

---
+++ Ch1_ArraysAndStrings/src/Q4_IsPalidromeOfPermutation_Array.java
@@ -0,0 +1,34 @@
+public class Q4_IsPalidromeOfPermutation_Array {
+
+    static boolean isPalidromeOfPermutation(String str) {
+        int countOdd = 0;
+        int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+        for (char c: str.toCharArray()) {
+            int index = getCharNumber(c);
+            if (index != -1) {
+                table[index]++;
+                if (table[index] % 2 == 1) {
+                    countOdd++;
+                } else
+                    countOdd--;
+            }
+        }
+        return countOdd <= 1;
+    }
+
+    static int getCharNumber(char c) {
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int val = Character.getNumericValue(c);
+        if (a <= val && val <= z) {
+            return val - a;
+        } else
+            return -1;
+    }
+    public static void main(String[] args) {
+        String[] phrases = {"Tact Coa", "Tact Ca", "Apple", ""};
+        for (String phrase: phrases) {
+            System.out.println(phrase + ": " + isPalidromeOfPermutation(phrase));
+        }
+    }
+}
