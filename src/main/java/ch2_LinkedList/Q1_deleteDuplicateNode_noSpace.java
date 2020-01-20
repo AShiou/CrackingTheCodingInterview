2806a532ec530392ce734e36e1ae40022f08b737
AShiou <aa8693@gmail.com>
2020-01-20 23:36:21 +0800

    Ch2-Q1 deleteDuplicateNode

---
+++ src/main/java/ch2_LinkedList/Q1_deleteDuplicateNode_noSpace.java
@@ -0,0 +1,41 @@
+package ch2_LinkedList;
+
+import library.LinkedListNode;
+
+import java.util.HashSet;
+import java.util.Random;
+
+public class Q1_deleteDuplicateNode_noSpace {
+    static void deleteDup(LinkedListNode head) {
+        LinkedListNode current = head;
+        while (current != null) {
+            LinkedListNode runner = surrent;
+            while (runner.next != null) {
+                if (runner.next.data  == current.data) {
+                    runner.next = runner.next.next;
+                } else {
+                    runner = runner.next;
+                }
+            }
+            current = current.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedListNode first = new LinkedListNode(0, null, null);
+        LinkedListNode head = first;
+        LinkedListNode second;
+        Random ran = new Random();
+        for (int i = 1; i < 10; i++) {
+            second = new LinkedListNode(ran.nextInt(10), null, null);
+            first.setNext(second);
+            second.setPrevious(first);
+            first = second;
+        }
+        System.out.println(head.print());
+        deleteDup(head);
+        System.out.println(head.print());
+    }
+}
+
+
