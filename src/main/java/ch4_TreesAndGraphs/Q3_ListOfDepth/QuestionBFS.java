30a909119c55b558db330d07453b75efdce7c2e7
AShiou <aa8693@gmail.com>
2020-02-13 22:09:41 +0800

    Ch4-Q3 ListOfDepth

---
+++ src/main/java/ch4_TreesAndGraphs/Q3_ListOfDepth/QuestionBFS.java
@@ -0,0 +1,49 @@
+package ch4_TreesAndGraphs.Q3_ListOfDepth;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class QuestionBFS {
+    static ArrayList<LinkedList<TreeNode>> createLevelLinkedList(TreeNode root) {
+        ArrayList<LinkedList<TreeNode>> result = new ArrayList<LinkedList<TreeNode>>();
+        LinkedList<TreeNode> current = new LinkedList<TreeNode>();
+        if (root != null) {
+            current.add(root);
+        }
+        while (current.size() > 0) {
+            result.add(current);
+            LinkedList<TreeNode> parents = current;
+            current = new LinkedList<TreeNode>();
+            for (TreeNode parent: parents) {
+                if (parent.left != null) {
+                    current.add(parent.left);
+                }
+                if (parent.right != null) {
+                    current.add(parent.right);
+                }
+            }
+        }
+        return result;
+    }
+
+    static void printResult(ArrayList<LinkedList<TreeNode>> result){
+        int depth = 0;
+        for(LinkedList<TreeNode> entry : result) {
+            Iterator<TreeNode> i = entry.listIterator();
+            System.out.print("Link list at depth " + depth + ":");
+            while(i.hasNext()){
+                System.out.print(" " + ((TreeNode)i.next()).val);
+            }
+            System.out.println();
+            depth++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        TreeNode root = TreeNode.createMinimalBST(array);
+        ArrayList<LinkedList<TreeNode>> list = createLevelLinkedList(root);
+        printResult(list);
+    }
+}
