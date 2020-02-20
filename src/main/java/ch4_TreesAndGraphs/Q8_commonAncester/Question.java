f418a5630c53b2855ab4b575c3d6e1431619a8f4
AShiou <aa8693@gmail.com>
2020-02-20 23:09:04 +0800

    Ch4-Q8 commonAncestor

---
+++ src/main/java/ch4_TreesAndGraphs/Q8_commonAncester/Question.java
@@ -0,0 +1,47 @@
+package ch4_TreesAndGraphs.Q8_commonAncester;
+
+public class Question {
+    static TreeNode commonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        Result r = commonAncestorHelper(root, p, q);
+        if (r.isAncestor) {
+            return r.node;
+        }
+        return null;
+    }
+
+    static Result commonAncestorHelper(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return new Result(null, false);
+
+        if (root == p && root == q) {
+            return new Result(root, true);
+        }
+
+        Result rx = commonAncestorHelper(root.left, p, q);
+        if (rx.isAncestor) {
+            return rx;
+        }
+
+        Result ry = commonAncestorHelper(root.right, p, q);
+        if (ry.isAncestor) {
+            return ry;
+        }
+
+        if (rx.node != null && ry.node != null) {
+            return new Result(root, true);
+        } else if (root == p || root == q) {
+            boolean isAncestor = rx.node != null || ry.node != null;
+            return new Result(root, isAncestor);
+        } else {
+            return new Result(rx.node != null ? rx.node : ry.node, false);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        TreeNode root = TreeNode.createMinimalBST(array);
+        TreeNode ancestor = commonAncestor(root, root.left, root.right);
+
+        System.out.println(ancestor.val);
+
+    }
+}
---
+++ src/main/java/ch4_TreesAndGraphs/Q8_commonAncester/Result.java
@@ -0,0 +1,10 @@
+package ch4_TreesAndGraphs.Q8_commonAncester;
+
+public class Result {
+    public TreeNode node;
+    public boolean isAncestor;
+    public Result(TreeNode n, boolean isAnc) {
+        node = n;
+        isAncestor = isAnc;
+    }
+}
